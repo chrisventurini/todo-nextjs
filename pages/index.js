b32bae0a2ce63de5da879168bd8cb5a3a22c04e5
Chris Venturini <chris.venturini@gmail.com>
2018-10-28 21:26:38 -0400

    Index clean up

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,13 @@
 import '../styles.scss';
 
-import { Component } from 'react';
-
 import Header from '../components/Header'
 import TodoList from '../components/ToDoList'
 
-class Index extends Component {
-
-    render() {
-       return (
-            <div>
-                <Header />
-                <TodoList />
-            </div>
-       )
-    }
-
+ export default () => {
+    return (
+        <div>
+            <Header />
+            <TodoList />
+        </div>
+   )
 }
-
-export default Index;
